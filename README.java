f7ba9a1686dcb68a93fd29c0c2e1185a972f67bc
ABHIMANYU BANSAL <47588250+bansalabhimanyu@users.noreply.github.com>
2019-09-19 19:09:25 +0530

    Create README.java

---
+++ README.java
@@ -0,0 +1,10 @@
+# Sample_java_programs
+Hello_world_program
+public class Hello_World
+{
+ public static void main(String[] args)//can also use (String args[])
+ { 
+ //to display 'Hello World'
+   System.out.println("Hello World");
+ }
+}
